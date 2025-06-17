875e737e4b2e2cc654927527e1adcc90fd3d1d4d
Paul Ross <apaulross@gmail.com>
2025-06-17 15:10:57 +0100

    WIP on thread_struct.py and unit tests.

---
+++ src/pprune/common/thread_struct.py
@@ -0,0 +1,129 @@
+import collections
+import dataclasses
+import datetime
+import re
+import string
+import typing
+
+# Matches 'http://www.pprune.org/tech-log/423988-concorde-question.html#post5866333'
+# Gives one group: ('5866333',)
+RE_PERMALINK_TO_POST_NUMBER = re.compile(r'\S+post(\d+)')
+
+# Matches "https://www.pprune.org/members/219249-nicolai"
+# To: ("219249", "nicolai")
+RE_USER_HREF_TO_USER_ID = re.compile(r'.+?/(\d+)-(\S+)')
+
+
+@dataclasses.dataclass
+class User:
+    """Represents a user. Taken from a node such as:
+    <a rel="nofollow" class="bigusername" href="https://www.pprune.org/members/219249-nicolai">nicolai</a>
+    """
+    href: str
+    name: str
+
+    @property
+    def user_id(self) -> typing.Optional[str]:
+        m = RE_USER_HREF_TO_USER_ID.match(self.href)
+        if m:
+            return f'{m.group(1)}-{m.group(2)}'
+
+    @property
+    def user_int(self) -> typing.Optional[int]:
+        m = RE_USER_HREF_TO_USER_ID.match(self.href)
+        if m:
+            return int(m.group(1))
+
+
+class Post:
+    """Represents a single post in a thread."""
+    PUNCTUATION_TABLE = str.maketrans({key: None for key in string.punctuation})
+
+    def __init__(self, timestamp: datetime.datetime, permalink: str, user: User, text: str,
+                 sequence_num: int):
+        self.timestamp = timestamp
+        self.permalink = permalink
+        self.user = user
+        self.text = text
+        self.sequence_num = sequence_num
+
+    def __str__(self):
+        return (
+            f'Timestamp: {self.timestamp},'
+            f' Permalink: {self.permalink},'
+            f' User: {self.user},'
+            f' Sequence: {self.sequence_num}'
+        )
+
+    @property
+    def text_stripped(self) -> str:
+        """The text in the node with blank lines and pre/post whitespace removed.
+        Inter-word spaces are maintained."""
+        ret = []
+        for line in self.text.split('\n'):
+            line = line.strip()
+            if len(line):
+                ret.append(line)
+        return '\n'.join(ret)
+
+    @property
+    def words(self) -> typing.List[str]:
+        txt = self.text_stripped.translate(self.PUNCTUATION_TABLE)
+        return txt.split()
+
+    @property
+    def post_number(self) -> typing.Optional[int]:
+        m = RE_PERMALINK_TO_POST_NUMBER.match(self.permalink)
+        if m is not None:
+            return int(m.group(1))
+
+    def words_removed(self, remove_these: typing.Set[str], lower_case: bool) -> typing.List[str]:
+        """Return the words in the post having removed all specified words and made lower case if required.
+        NOTE: The case of remove_words must match lower_case if that it to be effective."""
+        result = []
+        for w in self.words:
+            if lower_case and w.upper() != w:
+                w = w.lower()
+            if w not in remove_these:
+                result.append(w)
+        return result
+
+
+class Thread:
+    """Represents a thread of ordered posts with some internal indexing."""
+
+    def __init__(self):
+        # Ordered list of posts
+        self.posts: typing.List[Post] = []
+        # Map of {permalink : post_ordinal, ...}
+        self.post_map: typing.Dict[str, int] = {}
+        # Map of {User : [post_ordinal, ...], ...}
+        self.user_post_indexes: typing.Dict[User, typing.List[int]] = collections.defaultdict(list)
+
+    def __len__(self) -> int:
+        return len(self.posts)
+
+    def __getitem__(self, item) -> Post:
+        return self.posts[item]
+
+    def add_post(self, post: Post):
+        """Add a post."""
+        if post.permalink in self.post_map:
+            raise ValueError('permalink already in post_map. Trying to add: {:s}'.format(post.permalink))
+        self.post_map[post.permalink] = len(self.posts)
+        self.user_post_indexes[post.user].append(len(self.posts))
+        self.posts.append(post)
+
+    @property
+    def all_users(self) -> typing.Set[User]:
+        """All the users in this thread."""
+        return set([p.user for p in self.posts])
+
+    def get_post(self, permalink: str) -> Post:
+        """Given a permalink this returns the Post object corresponding to that permalink.
+        May raise KeyError or IndexError."""
+        return self.posts[self.post_map[permalink]]
+
+    def get_post_ordinals(self, user: User) -> typing.List[int]:
+        """Given a user what posts have they made, by ordinal."""
+        return self.user_post_indexes[user]
---
+++ tests/unit/common/test_thread_struct.py
@@ -0,0 +1,241 @@
+import datetime
+
+import pytest
+
+from pprune.common import thread_struct
+
+
+@pytest.mark.parametrize(
+    'permalink, expected',
+    (
+            ('http://www.pprune.org/tech-log/423988-concorde-question.html#post5866333', ('5866333',)),
+    )
+)
+def test_RE_PERMALINK_TO_POST_NUMBER(permalink, expected):
+    m = thread_struct.RE_PERMALINK_TO_POST_NUMBER.match(permalink)
+    assert m is not None
+    assert m.groups() == expected
+
+
+@pytest.mark.parametrize(
+    'href, expected',
+    (
+            ("https://www.pprune.org/members/219249-nicolai", ('219249', 'nicolai')),
+    )
+)
+def test_RE_USER_HREF_TO_USER_ID(href, expected):
+    m = thread_struct.RE_USER_HREF_TO_USER_ID.match(href)
+    assert m is not None
+    assert m.groups() == expected
+
+
+@pytest.mark.parametrize(
+    'href, name, expected',
+    (
+            ("https://www.pprune.org/members/219249-nicolai", 'nicolai', '219249-nicolai'),
+    )
+)
+def test_user_user_id(href, name, expected):
+    user = thread_struct.User(href, name)
+    assert user.user_id == expected
+
+
+@pytest.mark.parametrize(
+    'href, name, expected',
+    (
+            ("https://www.pprune.org/members/219249-nicolai", 'nicolai', 219249),
+    )
+)
+def test_user_user_int(href, name, expected):
+    user = thread_struct.User(href, name)
+    assert user.user_int == expected
+
+
+# NOTE: Typical permalink: "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338"
+
+@pytest.mark.parametrize(
+    'args, expected',
+    (
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This is the post text.",
+                            42,  # Sequence number
+                    ),
+                    'Timestamp: 2020-01-01 05:32:14, Permalink: https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338, User: nicolai, Sequence: 42',
+            ),
+    )
+)
+def test_post_ctor(args, expected):
+    post = thread_struct.Post(*args)
+    print()
+    print(post)
+    assert str(post) == expected
+
+
+@pytest.mark.parametrize(
+    'args, expected',
+    (
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This is the post text.",
+                            42,  # Sequence number
+                    ),
+                    'This is the post text.',
+            ),
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            """
+                            This is the post text.
+                            
+                            
+                            """,
+                            42,  # Sequence number
+                    ),
+                    'This is the post text.',
+            ),
+            # Check inter-word spacing.
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This   is    the post text.",
+                            42,  # Sequence number
+                    ),
+                    'This   is    the post text.',
+            ),
+    )
+)
+def test_post_text_stripped(args, expected):
+    post = thread_struct.Post(*args)
+    assert post.text_stripped == expected
+
+
+@pytest.mark.parametrize(
+    'args, expected',
+    (
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    ['This', 'is', 'the', 'post', 'text', 'See', ],
+            ),
+    )
+)
+def test_post_words(args, expected):
+    post = thread_struct.Post(*args)
+    assert post.words == expected
+
+
+@pytest.mark.parametrize(
+    'args, expected',
+    (
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    10994338,
+            ),
+    )
+)
+def test_post_post_number(args, expected):
+    post = thread_struct.Post(*args)
+    assert post.post_number == expected
+
+
+@pytest.mark.parametrize(
+    'args, remove_these, lower_case, expected',
+    (
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    set(),
+                    True,
+                    ['this', 'is', 'the', 'post', 'text', 'see'],
+            ),
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    set(),
+                    False,
+                    ['This', 'is', 'the', 'post', 'text', 'See'],
+            ),
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    {'this', 'the'},
+                    True,
+                    ['is', 'post', 'text', 'see'],
+            ),
+            # Example of when lower_case is False and the remove_these set is lower case. Some removal.
+            (
+                    (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                    ),
+                    {'this', 'the'},
+                    False,
+                    ['This', 'is', 'post', 'text', 'See'],
+            ),
+    )
+)
+def test_post_words_removed(args, remove_these, lower_case, expected):
+    post = thread_struct.Post(*args)
+    assert post.words_removed(remove_these, lower_case) == expected
+@pytest.mark.parametrize(
+    'posts, expected',
+    (
+            (
+                    [
+                        (
+                            datetime.datetime(2020, 1, 1, 5, 32, 14),
+                            "https://www.pprune.org/rumours-news/638797-united-b777-engine-failure.html#post10994338",
+                            'nicolai',
+                            "This, is the post text. See:",
+                            42,  # Sequence number
+                        )
+                    ],
+                    1,
+            ),
+    )
+)
+def test_thread(posts, expected):
+    thread = thread_struct.Thread()
+    for post_args in posts:
+        thread.add_post(thread_struct.Post(*post_args))
+    assert len(thread) == expected
